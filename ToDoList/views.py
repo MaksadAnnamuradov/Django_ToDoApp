434eb3fb44ee5dea44878dc752350b83a75584d2
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-12-09 11:05:52 -0700

    Added forms.py, and index view to post new ToDo.

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, HttpResponse
-
+from .forms import TaskForm
+from django.shortcuts import redirect
 # Create your views here.
 
+
 def index(request):
-    return render(request, "index.html")
+    # return HttpResponse("Hello World!!")
+    form = TaskForm()
+    if request.method == "POST":
+        #Get the posted form
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index.html")
+    return render(request, "index.html", {"task_form": form})
+
