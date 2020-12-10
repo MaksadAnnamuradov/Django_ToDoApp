448f9f604f1a6e55b2b79b7aa3d2fcf0f73aed38
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-12-10 10:07:17 -0700

    Added content, created, due_date fields for the Task model

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Task(models.Model):
     title=models.CharField(max_length=350)
     completed=models.BooleanField(default=False)
-    created=models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True)
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
 
     def __str__(self):
         return self.title
