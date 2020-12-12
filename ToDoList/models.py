c1548fe867522a8302844acd6b51fc5de8275fb5
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-12-11 17:47:32 -0700

    Category working !!! User can add category to the task

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 
+
+class Category(models.Model): # The Category table name that inherits models.Model
+    name = models.CharField(max_length=100, primary_key=True) #Like a varchar
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+    def __str__(self):
+        return self.name #name to be shown when called
+
+
+
 # Create your models here.
 class Task(models.Model):
     title=models.CharField(max_length=350)
@@ -9,6 +20,7 @@ class Task(models.Model):
     content = models.TextField(blank=True)
     created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    category = models.ForeignKey(Category, default="general", on_delete= models.CASCADE) # a foreignkey
 
     class Meta:
         ordering = ["-created"]
