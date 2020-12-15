ca46e46c36aeec7cde58aa554942575c02894db6
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2020-12-14 18:58:22 -0700

    Added some styling to edit page

--- ToDoList/forms.py
+++ ToDoList/forms.py
@@ -6,7 +6,15 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = "__all__"  # include all fields in form
+        fields = ('title', 'content', 'created', 'priority', 'due_date', 'category', 'completed',) # include all fields in form
         # fields=('title','completed') # include particular fileds of model in form
 
         #We can add widgets to forms to treat them like html elements
+        widgets = {
+            'title': forms.TextInput(attrs = {'class': 'form-control'}),
+            'content': forms.Textarea(attrs = {'class': 'form-control'}),
+            'created': forms.DateTimeInput(attrs={'readonly':'readonly'}),
+            'due_date': forms.DateTimeInput(attrs = {'class': 'form-control', 'type': 'datetime'}),
+            'completed': forms.CheckboxInput(),
+            'category': forms.Select(attrs = {'class': 'form-control'}),
+        }
